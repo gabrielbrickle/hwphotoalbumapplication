210375b4d51d047c8cfef421f48b01595d00b6b6
Gabriel Brickle <gabebrickle@gabriels-mbp.attlocal.net>
2016-06-05 20:38:01 -0400

    working on alignment for second page with album photos and on page with one large photo

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
 
   $('.albums').html(albumsStr); // add albums
 
+////Clicking on homepage buttons results in 6 data photos for specific city
+
   $('.albums').on('click', '.album', function (event) {
       console.log($(this).data('id'));
       var albumId = $(this).data('id');
@@ -53,7 +55,8 @@ $(document).ready(function () {
 
 })
 
-  //when you click on the title of the page it takes you back to the main albums list
+
+  //when you click on the title of the page it takes you back to the main albums list aka homepage
 
   $('h2').on('click', function(event) {
     event.preventDefault();
@@ -63,15 +66,18 @@ $(document).ready(function () {
     $('.albumDetail').addClass('hidden')
   })
 
+////showing the sidebar when you click on the homepage images///
 
   $('.albums').on('click', function(event){
     event.preventDefault();
     console.log(this);
     $('.sidebar').removeClass('hidden');
-
+    // $('h3').addClass('hidden');
+    // $('.albumDetail').css('display', 'flex');
+    // $('.albumDetail').css("margin-left", "0px"); //need images to be same css as other pages
   })
 
-  //need cover and li to function the same. on click hide main photos and active other 6 photos
+  //When you click a cover photo (homepage photo) the homepage photos hide and the album specific photos show
 
 $('.cover').on('click', function(event){
   event.preventDefault();
@@ -81,30 +87,65 @@ $('.cover').on('click', function(event){
 })
 
   //enlarging the photos
+  //lines below remove sidebar, header, h2, captions as of sunday 6/5
 
 $('.albumDetail').val('.img').on('click', function(event){
 event.preventDefault();
 var clickedThing = $(this).text();
   $('.sidebar').addClass('hidden');
-    // $('.header').removeClass('active');
-    // $('.albumDetail').removeClass('active');
+  $('h2').addClass('hidden');
+  $('h3').addClass('hidden');
+  $('.button').removeClass('hidden');
+  $('header').addClass('hidden');
+  $('.images').css("padding-top",'15px');
+  $('.images').css("margin-left",'0px');
+});
+
+
+/// using location icon to direct back to homepage
+
+$('.button').on('click', function(event) {
+  event.preventDefault();
+  console.log(this);
+  $('.albums').removeClass('hidden');
+  $('.sidebar').addClass('hidden');
+  $('.albumDetail').addClass('hidden');
+  $('header').addClass('active').removeClass('hidden');
+  $('.homebutton').removeClass('hidden');
+  $('.images').css("padding-top", '80px');
 })
 
-var correctPhoto = albums.filter(function(element) {
-  return correctPhoto === element.pictures
-})
-
-var albumsStr = '';
-correctPhoto[0].pictures.forEach(function (item, idx, arr) {
-  albumsStr += `<div class="album" ">
-                <img src="${item.photo}" alt="">
-
-              </div>`;
-})
-
-$('.albumDetail').html(albumsStr);
-
-
-
+///Attempt to only show one large image
+///Attemp #1
+// var albumsStr = '';
+// albums.forEach(function (item, idx, arr) {
+//   albumsStr += `<div class=".photoDetail" >
+//                 <img src="${item.photo}" alt="">
+//                 <h3>${item.caption}</h3>
+//               </div>`;
+// })
+// console.log(this);
+// $('.photoDetail').html(albumsStr);
+// $('.albums').on('click', '.album', function (event) {
+//     console.log($(this).data('id'));
+//Attempt #2
+// $('img').on('click', function (event) {
+//     console.log($(this).data('id'));
+//     var albumIds = $(this).data('id');
+//     var selectedAlbumPic = albums.filter(function (item, idx, arr) {
+//       return item.id === albumIds;
+//     })
+//     console.log("SELECTED PHOTO", selectedAlbumPic);
+//     var photosString = '';
+//     selectedAlbumPic[0].pictures.forEach(function (item, idx, arr) {
+//       photosString += `<div class="photo">
+//                       <img src="${item.photo}" alt="">
+//                     </div>`
+//     });
+//     // $('.albumDetail').addClass('active');
+//     $('.albums').addClass('hidden').removeClass('active');
+//     $('.albumDetail').html(photosString).removeClass('hidden').addClass('active');
+//
+//   })
 
 }) //Closing Tag
